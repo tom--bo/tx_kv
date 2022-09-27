60c4c2c73766aa1ed9443d9c639dcc8876bceadc
tom--bo <otsuka.tt@gmail.com>
2022-09-27 09:35:34 +0900

    Close #1, Impl Converting-lock

--- src/kv/LockManager.cpp
+++ src/kv/LockManager.cpp
@@ -5,7 +5,7 @@
 /* LockHash is not hashed, so specify key directly now */
 LockReply LockManager::Lock(TxCB *me, ulong key, LockMode mode) {
   LockHead *head; /* *lock in original TP-book */
-  LockRequest *request, *last;
+  LockRequest *request, *now, *prev;
 
   head = lockhash->FindLockHead(key);
   if(head == nullptr) {
@@ -25,37 +25,52 @@ LockReply LockManager::Lock(TxCB *me, ulong key, LockMode mode) {
     pthread_mutex_unlock(&head->mu); /* unlock here 01 */
     return LOCK_OK; /* equivalent to L:17 in lock() in TP-book(jp) p570 */
   } else { // has locks anyway L:18
-    last = head->queue;
+    now = head->queue;
+    /* check lock-conversion, and lock for same key from own tx */
     do {
-      /* check lock-conversion, and lock for same key from own tx */
-      if(last->txcb->txid == me->txid) {
-        if(last->mode > mode) {
+      if(now->txcb->txid == me->txid) {
+        if(now->mode >= mode) {
           // already has strong lock_mode lock
           pthread_mutex_unlock(&head->mu); /* unlock here 01 */
           delete(request);
           return LOCK_OK;
         } else {
           // request lock conversion
-          // ?? 自分以外のgranted_modeを再計算する
-          if(LockCompat(mode, recalc_granted_mode)) { /* Requested lock-mode is compatible with granted-lock mode */
-            last->mode = mode;
+          // recalc granted_mode without own lock
+          LockMode recalcGrantedMode = LOCK_FREE;
+          LockRequest *tmpReq = head->queue;
+          while(tmpReq != nullptr) {
+            if(tmpReq->txcb != me) {
+              recalcGrantedMode = GrantGroup(recalcGrantedMode, tmpReq->mode);
+            }
+            tmpReq = tmpReq->next;
+          }
+          if(LockCompat(mode, recalcGrantedMode)) { /* Requested lock-mode is compatible with recalculated granted-lock mode */
+            now->mode = mode;
             head->granted_mode = GrantGroup(mode, head->granted_mode);  /* lock_max() in original */
+            pthread_mutex_unlock(&head->mu); /* unlock here 01 */
             delete(request);
+            return LOCK_OK;
           } else {
             head->waiting = true;
-            last->convert_mode = mode;
+            now->convert_mode = mode;
             delete(request);
-            while(last->convert_mode != LOCK_FREE) {
+            while(now->convert_mode != LOCK_FREE) {
               pthread_cond_wait(&head->cond, &head->mu);
             }
+            pthread_mutex_unlock(&head->mu); /* unlock here 01 */
+            return LOCK_OK;
           }
         }
       }
-      last = last->next;
-    } while(last->next != nullptr);
+      prev = now;
+      now = now->next;
+    } while(now != nullptr);
+    /* End of checking lock-conversion, there is no lock request from same tx */
+    now = prev; // reset for next step
     if(!head->waiting && LockCompat(mode, head->granted_mode)) { /* L:25 This key has no waiting thread and current lock-req is also granted */
       head->granted_mode = GrantGroup(mode, head->granted_mode); /* lock_max() in original */
-      last->next = request;
+      now->next = request;
       if(me->anchor == nullptr) {
         me->listhead = request;
       } else {
@@ -67,7 +82,7 @@ LockReply LockManager::Lock(TxCB *me, ulong key, LockMode mode) {
     } else { /* L:30 This lock req should be queued because there is waiting thread or this lock-req is not compatible with granted lock mode */
       head->waiting = true;
       request->status = LOCK_WAITING;
-      last->next = request;
+      now->next = request;
       me->wait = request;
       while(request->status == LOCK_WAITING) {
         pthread_cond_wait(&head->cond, &head->mu);
@@ -86,10 +101,12 @@ LockReply LockManager::Lock(TxCB *me, ulong key, LockMode mode) {
 }
 
 bool LockManager::Unlock(LockRequest *req) {
-  LockRequest *now, *priv;
+  LockRequest *now, *prev;
+  TxCB *convertingTxCB = nullptr;
   LockHead *lockhead = req->head;
+  bool hasConvertingLock = false;
   pthread_mutex_lock(&lockhead->mu); /* lock 01 */
-    if(lockhead->queue == req && req->next == nullptr) { /* L:19 in UnLock in TP-book(jp) p572 */
+  if(lockhead->queue == req && req->next == nullptr) { /* L:19 in UnLock in TP-book(jp) p572 */
     /* In my impl, do not release lockheader */
     lockhead->queue = nullptr;
     lockhead->waiting = false;
@@ -101,33 +118,63 @@ bool LockManager::Unlock(LockRequest *req) {
       // The second and subsequent in lockhead->queue is 'req'
       now = lockhead->queue;
       while(now != nullptr && now != req) {
-        priv = now;
+        prev = now;
         now = now->next;
       }
       if(now == nullptr) {
-        // TBD: error
+        // TBD: bug
         exit(2);
       }
-      priv->next = now->next; /* delete req node from list */
+      prev->next = now->next; /* delete req node from list */
     }
+    /* Find converting-lock request only the first one */
+    now = (lockhead->queue);
+    while(now != nullptr && now->status == LOCK_GRANTED) {
+      if(now->convert_mode != LOCK_FREE) {
+        hasConvertingLock = true;
+        convertingTxCB = now->txcb;
+        break;
+      }
+      now = now->next;
+    }
+
     /* re-calc granted_mode */
     lockhead->granted_mode = LOCK_FREE;
     now = (lockhead->queue);
     while(now != nullptr && now->status == LOCK_GRANTED) {
-      /* granted_mode is not updated correctly */
-      lockhead->granted_mode = GrantGroup(now->mode, lockhead->granted_mode);
+      if(now->txcb != convertingTxCB) {
+        lockhead->granted_mode = GrantGroup(now->mode, lockhead->granted_mode);
+      }
       now = now->next;
     }
-    /* Unlock other locks according to granted_mode */
-    while(now != nullptr && LockCompat(now->mode, lockhead->granted_mode)) {
-      /* granted_mode is not updated correctly */
-      lockhead->granted_mode = GrantGroup(now->mode, lockhead->granted_mode);
-      now->status = LOCK_GRANTED;
-      if(now->next == nullptr) {
-        lockhead->waiting = false;
-        break;
-      } else {
+    /* Skip this if converting-lock was not found */
+    if(hasConvertingLock) {
+      /* [1] Unlock waiting convert-lock according to granted_mode */
+      // TBD: Fix this loop LOCK_GRANTED locks (for performance)
+      now = (lockhead->queue);
+      while(now != nullptr && now->status == LOCK_GRANTED) {
+        if(now->convert_mode != LOCK_FREE
+            && LockCompat(lockhead->granted_mode, now->convert_mode))
+        {
+          lockhead->granted_mode = GrantGroup(now->convert_mode, lockhead->granted_mode);
+          now->mode = now->convert_mode;
+          now->convert_mode = LOCK_FREE;
+          break;
+        }
         now = now->next;
+      }
+    } else {
+      /* [2] Unlock other waiting locks according to granted_mode, this will be skipped if [1] was exist. */
+      while(now != nullptr && LockCompat(now->mode, lockhead->granted_mode)) {
+        /* granted_mode is not updated correctly */
+        lockhead->granted_mode = GrantGroup(now->mode, lockhead->granted_mode);
+        now->status = LOCK_GRANTED;
+        if(now->next == nullptr) {
+          lockhead->waiting = false;
+          break;
+        } else {
+          now = now->next;
+        }
       }
     }
   }
@@ -135,6 +182,7 @@ bool LockManager::Unlock(LockRequest *req) {
   pthread_cond_broadcast(&lockhead->cond);
   return true;
 }
+
 bool LockManager::UnlockAll(TxCB *txcb) {
   LockRequest *req = txcb->listhead;
   LockRequest *next;
@@ -152,7 +200,7 @@ bool LockManager::LockCompat(LockMode requested, LockMode granted) {
     return 0; /* more than 6 (LOCK_WAIT) is error actually */
   }
   bool compatibility_matrix[7][7] {
-      0,0,0,0,0,0,0, /* LOCK_FREE is error actually */
+      1,1,1,1,1,1,1,
       1,1,1,1,1,0,0,
       1,1,1,0,0,0,0,
       1,1,0,1,0,0,0,
